0745dec86345885e221a3c9d6e9ca5fdc234919e
moirajm <mmunozm3@icloud.com>
2023-11-02 17:20:48 -0300

    praying

---
+++ src/Pages/PageTwo.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col"
+
+const Pagetwo = () => {
+    return (
+        <div className="container px-4 text-center">
+            <Row className="row gx-5">
+                <Col>
+                <h2>Es posible hacer el cambio. 
+            Ya lo hemos hecho antes.</h2>
+                </Col>
+                <Col>
+                <p>Nuestra misión es crear una red de ayuda para afrontar uno de los mayores retos de nuestra sociedad.  Nos esforzamos por construir un mundo donde cada individuo pueda gozar del derecho humano fundamental de no pasar hambre.</p>
+                </Col>
+            </Row>
+        </div>
+    );
+};
+
+export default PageTwo;
