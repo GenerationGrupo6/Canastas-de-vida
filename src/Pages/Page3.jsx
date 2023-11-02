73b96354083ed7d285497f5240b918b8b0c313a9
Felipe <Felipefriasmorales@gmail.com>
2023-11-02 16:01:45 -0300

    footer y navbar + orden de carpetas

---
+++ src/Pages/Page3.jsx
@@ -0,0 +1,15 @@
+
+
+const Page3 = () => {
+    return (
+        <>
+            <main >
+                <h2>
+                    hola mundo
+
+                </h2>
+            </main>
+        </>
+    )
+}
+export default Page3
