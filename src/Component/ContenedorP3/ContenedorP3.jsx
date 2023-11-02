829d66945432ef6c9940262dfff1c931cd3b76cd
Felipe <Felipefriasmorales@gmail.com>
2023-11-02 20:34:25 -0300

    pagina 3 lista

---
+++ src/Component/ContenedorP3/ContenedorP3.jsx
@@ -0,0 +1,23 @@
+import imagenCentral from "../assets/CanastaBasica1.jpg";
+import "./ContenedorP3.css";
+
+const ContenedorP3 = () => {
+    return (
+        <div className="contenedorPadre">
+            <div className="contenedor-Beige">
+                <div className="contenedor-Contenido">
+                    <h2 className="destacado">Conoce nuestras historias</h2>
+                    <p id="parrafo">
+                    Nuestra misión es crear una red de ayuda para afrontar uno de los
+                    mayores retos de nuestra sociedad. Nos esforzamos por construir un
+                    mundo donde cada individuo pueda gozar del derecho humano
+                    fundamental de no pasar hambre.
+                    </p>
+                    <button className="btn">Ver más historias</button>
+                </div>
+                <img src={imagenCentral} alt="imagenPag2" id="imagenPag2" />
+            </div>
+           </div>
+    )
+}
+export default ContenedorP3
